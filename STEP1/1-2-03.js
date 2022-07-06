a9088674ccc63d11c66e39c0df3b1a2d5ed23de2
ShogoOnda <duo0116@gmail.com>
2022-07-06 13:32:07 +0000

    [Add] 1-2-03.JS

---
+++ STEP1/1-2-03.js
@@ -0,0 +1,16 @@
+var displayElm = document.getElementsByClassName('display')[0];
+//startButtonというclassが付いているタグ要素のうち、
+//最初のもの（スタートボタン）を取り出す
+var startButton = document.getElementsByClassName('startButton')[0];
+
+//取り出したstartButtonに対してクリックイベントのリスナを仕掛ける
+startButton.addEventListener('click', function() {
+ //この行はクリックした時よばれる
+  console.log('start');
+  var seconds = 0;
+  setInterval(function(){
+    seconds++;
+    displayElm.innerText = seconds;
+    console.log(seconds);
+  }, 1000);
+});
